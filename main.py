22f908e2cbef3b94a6410bf15fb79e0ea679b660
Sumayer Khan Sajid <139729797+SumayerKhan@users.noreply.github.com>
2025-06-27 10:13:49 +0600

    Showing in the streamlit

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import requests
 import streamlit as st
 
+# API
 api_key = "a0OPdteZmUhcFEJ2qsQdPRUclZhwPd5ItF9gjYK1"
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
+# Fetching the data from API
 response = requests.get(url)
 content = response.json()
 
 title = content["title"]
 description = content["explanation"]
 image_url = content["hdurl"]
-image_response = requests.get(image_url)
 
+# Showing in the streamlit
+st.title("Astronomy Photo of the Day")
+st.header(title)
+st.image(image_url)
+st.write(description)
 
-with open("image.jpg", "wb") as photo:
-    photo.write(image_response.content)
